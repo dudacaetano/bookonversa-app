9e712c0d4c0180bae4ef6de4855af1203da1da2d
dudacaetano <mariaeduardascaetano@gmail.com>
2024-06-07 14:58:07 -0300

    Initial Commit

---
+++ bookonversa/bookonversaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookonversaApp.swift
+//  bookonversa
+//
+//  Created by Maria Eduarda on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct bookonversaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
